2f35945b77788d7061264a411a2008afce95f38a
Constantinos Dimitriou <constd@outlook.com>
2021-09-01 20:42:12 -0700

    more organized audioset

--- audar/audioset.py
+++ audar/audioset.py
@@ -5,12 +5,55 @@ from csv import reader, DictReader
 from multiprocessing import pool
 from tqdm import tqdm
 from utils import download_file, download_ytaudio
-from typing import List, Optional
+from typing import List, Optional, Any, Tuple, Union
 from glob import glob
 from os.path import splitext
+from dataclasses import dataclass, field
 
 
 AUDIOSET_ONTOLOGY_URL = "https://raw.githubusercontent.com/audioset/ontology/master/ontology.json"
+
+
+@dataclass
+class AudiosetClass:
+    id: Union[str, None] = field(default=None) 
+    name: Union[str, None] = field(default=None)
+    description: Union[str, None] = field(default=None)
+    citation_uri: Union[str, None] = field(default=None)
+    positive_examples: List[Optional[str]] = field(default_factory=list)
+    child_ids: List[Optional[str]] = field(default_factory=list)
+    restrictions: List[Optional[str]] = field(default_factory=list)
+
+
+class AudiosetOntology:
+    def __init__(self, path: str=AUDIOSET_ONTOLOGY_URL) -> None:
+        self.ontology_list = self.retrieve_ontology(path)
+        self.id_to_class = {
+            x.id: x for x in self.ontology_list
+        }
+
+    @staticmethod
+    def retrieve_ontology(path: str) -> List[AudiosetClass]:
+        if not os.path.exists(AUDIOSET_ONTOLOGY_URL.split("/")[-1]):
+            download_file(AUDIOSET_ONTOLOGY_URL)        
+        with open(AUDIOSET_ONTOLOGY_URL.split("/")[-1], "r") as f:
+            ontology = json.load(f)
+        return [AudiosetClass(**x) for x in ontology]
+
+    def items(self) -> Tuple[str, AudiosetClass]:
+        for k, v in self.id_to_class.items():
+            yield k, v
+
+    def _id_to_class(self, id):
+        return self.id_to_class.get(id, AudiosetClass())
+
+    def __call__(self, class_id: Union[str, List[str]]) -> str:
+        if type(class_id) == str:
+                return self._id_to_class(class_id).name
+        return [self._id_to_class(class_id).name for x in class_id]
+
+    def __len__(self) -> int:
+        return len(self.ontology_list)
 
 
 class AudiosetAnnotationReaderV1(DictReader):
@@ -61,36 +104,22 @@ class AudiosetAnnotationReaderV1(DictReader):
 
 
 class AudiosetSegment:
-    def __init__(self, segment_path: str, class_filters: List[Optional[str]]=[], ontology: dict=None):
+    def __init__(self, segment_path: str, ontology: AudiosetOntology, class_filters: List[Optional[str]]=[]):
         self.class_filters = class_filters
-        self.ontology = self.__load_ontology(ontology)
+        self.ontology = ontology
         with open(segment_path, 'r') as f:
             annotation_reader = AudiosetAnnotationReaderV1(f)
             self.annotations = [x for x in annotation_reader]
         if self.class_filters:
             self.annotations = self.filter_annotations(self.annotations)
 
-    @staticmethod
-    def __load_ontology(ontology):
-        if ontology is not None:
-            return {x['name'].lower(): x['id'] for x in ontology}
-        return ontology
-
     def filter_annotations(self, annotations):
         annotations_filtered = []
-        class_ids = [v for k, v  in self.ontology.items() if k in self.class_filters]
+        class_ids = [k for k, v  in self.ontology.items() if v.name.lower() in self.class_filters]
         for annotation in annotations:
             if set(class_ids) & set(annotation.get('positive_labels', [])):
                 annotations_filtered.append(annotation)
         return annotations_filtered
-
-
-def retrieve_ontology():
-    if not os.path.exists(AUDIOSET_ONTOLOGY_URL.split("/")[-1]):
-        download_file(AUDIOSET_ONTOLOGY_URL)        
-    with open(AUDIOSET_ONTOLOGY_URL.split("/")[-1], "r") as f:
-        ontology = json.load(f)
-    return ontology
 
 
 # def download_audioset_segment(segment_url, output_dir):
@@ -104,7 +133,7 @@ def retrieve_ontology():
 
 
 def download_audioset_segment(segment_url, output_dir, class_filters):
-    ontology = retrieve_ontology()
+    ontology = AudiosetOntology()
     download_file(segment_url)
     as_seg = AudiosetSegment(segment_url.split("/")[-1], class_filters=class_filters, ontology=ontology)
     existing_files = [splitext(x.split('/')[-1])[0].replace('.info', '') for x in glob(f"{output_dir}/*.json")]
